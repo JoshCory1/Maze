f37f6f45571315ab2f78f31dda2605500778a323
Joshua Cory <accounts@keeplooking.net>
2022-09-13 09:53:05 -0700

    Simple mase and mouse game.

---
+++ Assets/Mover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+   [SerializeField] float xValue = 0.00f;
+   [SerializeField] float yValue = 0.02f;
+   [SerializeField] float zValue = 0.00f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Translate(xValue,yValue,zValue);
+    transform.Translate(Input.GetAxis("Horizontal"),yValue,zValue);
+    }
+}
