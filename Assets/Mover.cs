b438de62f6fbed8bcf2ee0207a1e0e1b594c1987
Joshua Cory <accounts@keeplooking.net>
2022-09-14 16:07:59 -0700

    Added change color to walls and made a simple Debug.Log scorer.

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -7,17 +7,28 @@ public class Mover : MonoBehaviour
     float moveSpeed = 100f;
     void Start()
     {
-        
+      PrintInstructions();  
     }
 
     // Update is called once per frame
     // Time.deltaTime
     void Update()
     {
-        // transform.Translate(xValue,yValue,zValue);
-    
-    float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-    float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-    transform.Translate(xValue,0,zValue);
+        MovePlayer();
     }
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game, Move with (WASD)");
+        Debug.Log("get the mouse to the end of maze, Don't hit stuff sfuff is bad");
+        Debug.Log("Good luck");
+
+    }
+    void MovePlayer()
+    {
+         // transform.Translate(xValue,yValue,zValue);
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue,0,zValue);
+    }
+    
 }
---
+++ Assets/Scorer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+  void OnCollisionEnter(Collision other) 
+  {
+  Debug.Log("you have bumped into stuff:");
+  }
+}
